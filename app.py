e4b02b221592f823e5ac5a5091fa7e146e4808e2
Alejandro Ouslan <alejandro.ouslan@upr.edu>
2024-03-26 14:39:53 -0400

    added basic webapp template
    Signed-off-by: Alejandro Ouslan <alejandro.ouslan@upr.edu>

---
+++ app.py
@@ -0,0 +1,32 @@
+# Import packages
+from dash import Dash, html, dash_table, dcc, callback, Output, Input
+import pandas as pd
+import plotly.express as px
+
+# Incorporate data
+df = pd.read_csv('data/processed/idh_index.csv')
+
+# Initialize the app
+app = Dash(__name__)
+
+# App layout
+app.layout = html.Div([
+    html.Div(children='The IDH Index'),
+    html.Hr(),
+    dcc.RadioItems(options=['IDH', 'Income', 'Health', 'Education'], value='lifeExp', id='controls-and-radio-item'),
+    dash_table.DataTable(data=df.to_dict('records'), page_size=6),
+    dcc.Graph(figure={}, id='controls-and-graph')
+])
+
+# Add controls to build the interaction
+@callback(
+    Output(component_id='controls-and-graph', component_property='figure'),
+    Input(component_id='controls-and-radio-item', component_property='value')
+)
+def update_graph(col_chosen):
+    fig = px.histogram(df, x='continent', y=col_chosen, histfunc='avg')
+    return fig
+
+# Run the app
+if __name__ == '__main__':
+    app.run(debug=True)
